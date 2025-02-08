184a0afda79dfb1c7376aa92da5058de99af6365
J-ohnP-aul <johnpaulnoblemen@gmail.com>
2025-02-08 08:48:24 +0300

    worked with list looping through list and list compreheension range

--- composure/list.py
+++ composure/list.py
@@ -14,3 +14,27 @@ motorcycles = ['honda', 'yamaha', 'suzuki']
 popedMoto = motorcycles.pop(0)
 print(motorcycles)
 print(popedMoto)
+
+for friend in friends:
+    print(friend.title() + " You are among the friends I have never had")
+
+print("\nThank you, everyone. That was a great magic show!")
+
+dreams = ['pilot', 'senior Dev', 'leader', 'billionair', 'rugby', 'noble', 'Hacker'] 
+for me in dreams:
+    print("hello "+ me.title())
+
+print("a well defined potential for far more beyond this")
+
+numbers = list(range(1,20,2))
+print(numbers)
+#list comprehension
+squares = []
+for value in range(1,11):
+    square = value**2
+    squares.append(square)
+
+print(sum(squares))
+
+squares = [value**2 for value in range(1,11)]
+print(squares)
